17098841fe117a742fd427a6236a051a44a2c96f
Rafapszz <rafael.ruivo.pontes@escola.pr.gov.br>
2026-02-20 07:51:54 -0300

    Create pinpong.js

---
+++ pinpong.js
@@ -0,0 +1,59 @@
+
+// Criar Bolinha
+2
+ let raio = 7.5;
+3
+ let xBolinha = 100;
+4
+ let yBolinha = 200;
+5
+ let diametro = raio * 2;
+6
+​
+7
+// Velocidade da Bolinha
+8
+ let velocidadeXBolinha = 6;
+9
+ let velocidadeYBolinha = 6;
+10
+​
+11
+// Minha Raquete
+12
+ let xRaquete =5;
+13
+ let yRaquete = 150;
+14
+​
+15
+// Raquete Oponente
+16
+ let xRaqueteOponente = 585;
+17
+ let yRaqueteOponente = 150;
+18
+​
+19
+// Placar
+20
+ let meusPontos = 0;
+21
+ let pontosOponente = 0;
+22
+​
+
+function setup() {
+24
+  createCanvas(600, 400);
+25
+}
+There are no lint messages
+Current lineline 3
+
+Console
+Clear
+
+​
+Preview
+
