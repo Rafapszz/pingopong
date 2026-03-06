bcdddc570ef1f7b14cbd7bef94669a22a2722bd9
Rafapszz <rafael.ruivo.pontes@escola.pr.gov.br>
2026-03-06 07:54:15 -0300

    update

--- pinpong.js
+++ pinpong.js
@@ -1,59 +1,109 @@
+//Criar Bolinha
+let raio = 7.5;
+let xBolinha = 100;
+let yBolinha = 200;
+let diametro = raio * 2;
 
-// Criar Bolinha
-2
- let raio = 7.5;
-3
- let xBolinha = 100;
-4
- let yBolinha = 200;
-5
- let diametro = raio * 2;
-6
-​
-7
-// Velocidade da Bolinha
-8
- let velocidadeXBolinha = 6;
-9
- let velocidadeYBolinha = 6;
-10
-​
-11
-// Minha Raquete
-12
- let xRaquete =5;
-13
- let yRaquete = 150;
-14
-​
-15
-// Raquete Oponente
-16
- let xRaqueteOponente = 585;
-17
- let yRaqueteOponente = 150;
-18
-​
-19
-// Placar
-20
- let meusPontos = 0;
-21
- let pontosOponente = 0;
-22
-​
+//Velocidade da bolinha
+let velocidadeXBolinha = 6;
+let velocidadeYBolinha = 6;
 
-function setup() {
-24
-  createCanvas(600, 400);
-25
+//Minha Raquete
+let xRaquete = 5;
+let yRaquete = 150;
+
+//Raquete Oponente
+let xRaqueteOponente = 585;
+let yRaqueteOponente = 150;
+
+//Placar
+let meusPontos = 0;
+let pontosOponentes = 0;
+
+//dificuldade
+let dificuldade = "normal";
+let trilha,ponto,raquetada;
+
+function preload(){
+  trilha = loadSound("trilha.mp3")
+  ponto = loadSound("ponto.mp3")
+  raquetada = loadSound("raquetada.mp3")
 }
-There are no lint messages
-Current lineline 3
 
-Console
-Clear
+function setup(){
+  createCanvas(600,400);
+  trilha.loop()
+}
 
-​
-Preview
+function draw(){
+  background(0)
+  movimentoBolinha();
+  raquetes(xRaquete,yRaquete);
+  raquetes(xRaqueteOponente,yRaqueteOponente);
+  npc(dificuldade);
+ 
+  if(keyIsDown(UP_ARROW)){
+    yRaquete -= 10}
+ 
+ 
+  if(keyIsDown(DOWN_ARROW)){
+    yRaquete += 10}
+ 
+  if(xBolinha -raio < xRaquete + 10 &
+     yBolinha -raio< yRaquete + 90 &
+     yBolinha + raio > yRaquete ){
+    velocidadeXBolinha *= -1;
+    raquetada.play()}
+  if(xBolinha + raio > xRaqueteOponente &
+     yBolinha - raio< yRaqueteOponente + 90 &
+     ybolinha + raio > yRaqueteOponente ){
+    velocidadeXBolinha *= -1;
+    raquetada.play()}
+ 
+  textSize(18);
+  textAlign(CENTER);
+  fill(color(255,0,233));
+  rect(150,10,40,20);
+  fill(255);
+  text(meusPontos,170,26);
+  fill(color(255,0,233));
+  rect(450,10,40,20);
+  fill(255);
+  text(pontosOponentes,470,26);
+ 
+  if(xBolinha>595){
+    meusPontos += 1;
+    ponto.play()
+  }
+    if(xBolinha<10){
+    pontosOponentes += 1;
+    ponto.play()
+  }
+}  
 
+function movimentoBolinha(){
+  circle(xBolinha,yBolinha,diametro);
+
+    xBolinha += velocidadeXBolinha;
+    yBolinha += velocidadeYBolinha
+    if(xBolinha + raio > width || xBolinha-raio < 0){
+      velocidadeXBolinha *= -1;
+    }
+    if(yBolinha + raio > height || yBolinha - raio < 0){
+      velocidadeYBolinha *= -1}
+}
+function raquetes(xRaquete,yRaquete){
+  rect(xRaquete,yRaquete,10,90)
+}
+
+function npc(){
+  var nivel = 30;
+  if(dificuldade === "Fácil"){
+    nivel = 100;
+  }
+  if(dificuldade === "Normal"){
+    nivel = 30
+  }
+  let velocidadeYRaqueteOponente = yBolinha - yRaqueteOponente - 10/2;
+  yRaqueteOponente += velocidadeYRaqueteOponente - nivel;
+}
